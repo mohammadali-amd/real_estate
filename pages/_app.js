55cf44c7e7e73c507772ac766951d04c28f6a22b
mohammadali-amd <amidi1380@gmail.com>
2023-03-10 23:17:59 +0330

    Add Progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,21 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export default function App({ Component, pageProps }) {
+  NProgress.configure({showSpinner: false});
+  
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+  });
+
+  Router.events.on('hashChangeComplete', () => {
+    NProgress.done();
+  });
+
   return (
     <>
       <Head>
         <title>Real State</title>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
       </Head>
       <header>
         <Navbar />
