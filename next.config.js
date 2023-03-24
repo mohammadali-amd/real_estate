fd49f3aa0c79dfd31fccdb051bf72ebbe1a2771d
mohammadali-amd <amidi1380@gmail.com>
2023-03-24 20:57:38 +0430

    install mui

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: 'standalone',
   // reactStrictMode: true,
+  experimental: {
+    appDir: false,
+  },
   images:{
     domains:['bayut-production.s3.eu-central-1.amazonaws.com']
 },
